f7c70411f61a21076b97cadb4ec1e6d8c16f16b4
Zach Xu <zach.popping@gmail.com>
2022-12-28 16:45:46 -0500

    Backend setup

---
+++ src/main/java/com/zach/ZaRateApplication.java
@@ -0,0 +1,13 @@
+package com.zach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaRateApplication.class, args);
+    }
+
+}
