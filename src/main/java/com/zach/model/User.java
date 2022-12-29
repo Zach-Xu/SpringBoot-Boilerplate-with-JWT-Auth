85448229c417e6fb3f3eda7a1b7218d5e39454c6
Zach Xu <zach.popping@gmail.com>
2022-12-28 19:24:07 -0500

    Create User and UserInfo model

---
+++ src/main/java/com/zach/model/User.java
@@ -0,0 +1,39 @@
+package com.zach.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "User")
+@Table(name="tb_user")
+public class User {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    @Column(name = "username", nullable = false, columnDefinition = "VARCHAR(32)")
+    private String username;
+
+    @Column(name="phone", columnDefinition = "VARCHAR(10) DEFAULT ''")
+    private String phone;
+
+    @Column(name="avatar", columnDefinition = "VARCHAR(255) DEFAULT ''")
+    private String avatar;
+
+    @Column(name="create_time", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createTime;
+
+    @Column(name="update_time", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updateTime;
+
+    @OneToOne(
+            mappedBy = "user",
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE}
+    )
+    private UserInfo userInfo;
+}
---
+++ src/main/java/com/zach/model/UserInfo.java
@@ -0,0 +1,54 @@
+package com.zach.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "UserInfo")
+@Table(name = "tb_user_info")
+public class UserInfo {
+
+    @Id
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @MapsId
+    @JoinColumn(
+            name = "user_id",
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "user_info__user_fk"
+            )
+    )
+    private User user;
+
+    @Column(name = "city", columnDefinition = "VARCHAR(64) DEFAULT ''")
+    private String city;
+
+    @Column(name = "introduction", columnDefinition = "VARCHAR(128) DEFAULT NULL COMMENT 'self introduction, no more than 128 characters'")
+    private String intro;
+
+    @Column(name = "fans", columnDefinition = "INT(8) UNSIGNED DEFAULT 0 COMMENT 'Number of fans'")
+    private int fans;
+
+    @Column(name = "followings", columnDefinition = "INT(8) UNSIGNED DEFAULT 0 COMMENT 'Number of followings'")
+    private int followings;
+
+    @Column(name = "gender", columnDefinition = "TINYINT(1) UNSIGNED DEFAULT 0 COMMENT 'Gender, 0:Male, 1:Female'")
+    private int gender;
+
+    @Column(name = "birthday", columnDefinition = "DATE DEFAULT NULL")
+    private LocalDate birthday;
+
+    @Column(name = "credits", columnDefinition = "INT(8) DEFAULT 0 COMMENT 'points earned' ")
+    private int credits;
+
+    @Column(name = "tier", columnDefinition = "TINYINT(1) DEFAULT 0 COMMENT 'Membership tier, 0-9, 0 means unsubscribed'")
+    private int tier;
+
+    @Column(name = "create_time", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createTime;
+
+    @Column(name = "update_time", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updateTime;
+}
